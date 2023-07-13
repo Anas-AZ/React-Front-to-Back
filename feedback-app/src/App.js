e6eb422e107505843984f310b7b630db4bfe76ea
Anas <anas9991az@gmail.com>
2023-07-13 14:50:29 +0530

    added props

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,40 +1,16 @@
+import Header from "./components/Header"
+
 function App(){
-
-    const title = 'Blog Post'
-    const body  = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'comment one'},
-        {id: 2, text: 'comment two'},
-        {id: 3, text: 'comment three'}
-    ]
-
-    const loading = false
-    const showComments = true
-
-    const commentBlock = (
-                <div classname="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map(
-                        (comment, index) => (
-                            <li key={index}>{comment.text}</li>
-                        )
-                    )}
-                </ul>
-            </div>
-    )
-
-    if(loading) return <h1>Loading...</h1>
-
-        return (
+    return(
+        <>
+            <Header/>
             <div className="container">
-                <h1>{title.toUpperCase()}</h1>
-                <p>{body}</p>
-
-            {showComments && commentBlock}
-            
+            <h1>My APP</h1>
             </div>
-        )
+        </>
+    )
+   
 }
+
 
 export default App
